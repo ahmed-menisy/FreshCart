3ad8d9997cdd8f4c22ed1b70f60d57e6181aa615
ahmed-menisy <amelmaster16@gmail.com>
2023-09-20 03:52:13 +0200

    upload2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   baseUrlAuth: `https://ecommerce.routemisr.com/api/v1/auth`,
   baseUrlData: `https://ecommerce.routemisr.com/api/v1`,
-  payUrl: 'http://localhost:4200',
+  payUrl: 'https://ahmed-menisy.github.io/FreshCart/#',
 };
